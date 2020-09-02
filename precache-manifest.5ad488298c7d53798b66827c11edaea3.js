ca8064e49b5b8053814010917966e101ef6d395b
Kate Kladova <ek.kladova@gmail.com>
2020-09-02 17:44:58 +0100

    Updates

---
+++ precache-manifest.5ad488298c7d53798b66827c11edaea3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9655a871a12fa4a892a348b980a6c5cd",
+    "url": "/portfolioCV/index.html"
+  },
+  {
+    "revision": "27831edea99c3c3bf4eb",
+    "url": "/portfolioCV/static/css/main.66da3788.chunk.css"
+  },
+  {
+    "revision": "39cf73761ceefd3665f0",
+    "url": "/portfolioCV/static/js/2.987003a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioCV/static/js/2.987003a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27831edea99c3c3bf4eb",
+    "url": "/portfolioCV/static/js/main.7c193ff7.chunk.js"
+  },
+  {
+    "revision": "1ce3470fc222cf225953",
+    "url": "/portfolioCV/static/js/runtime-main.811e3db7.js"
+  },
+  {
+    "revision": "d3ac5fd1e372e8ad8fd5402b0a1c4472",
+    "url": "/portfolioCV/static/media/cv.d3ac5fd1.pdf"
+  },
+  {
+    "revision": "2c6238e304700a77fcda3ae44fad0590",
+    "url": "/portfolioCV/static/media/game.2c6238e3.png"
+  },
+  {
+    "revision": "db43abdf275f04715bf5380c2da1f51b",
+    "url": "/portfolioCV/static/media/screen.db43abdf.gif"
+  },
+  {
+    "revision": "e8bdbd45419c30eac4a5ca555869bef4",
+    "url": "/portfolioCV/static/media/timer.e8bdbd45.PNG"
+  },
+  {
+    "revision": "8eb4629bde6a28743c8c7124bc1d185e",
+    "url": "/portfolioCV/static/media/weather.8eb4629b.png"
+  },
+  {
+    "revision": "39e173bd84367043dcda3062a738097f",
+    "url": "/portfolioCV/static/media/web.39e173bd.png"
+  },
+  {
+    "revision": "0b4fc648d84d27b8140676e2d7b91058",
+    "url": "/portfolioCV/static/media/website.0b4fc648.PNG"
+  }
+]);
